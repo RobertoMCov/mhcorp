6cde6ac839be29c2509a186a8e9d48dde3c08f08
RobertoMCov <rmiranda@covalu.com>
2024-12-07 21:06:26 -0600

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
-  <App />,
+  <>
+  <App />
+  <p>El comienzo de mhcorp equisde papu</p>
+  </>,
 )
