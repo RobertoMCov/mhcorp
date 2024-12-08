c15d0c7787b38e4abc1365cf83284764ff53e340
RobertoMCov <rmiranda@covalu.com>
2024-12-07 20:37:08 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="text-xl p-2 text-center">
+        El comienzo de algo grande
+      </div>
+      <p className="p-2 text-center">MH - CORP :v </p>
+    </>
+  )
+}
+
+export default App
